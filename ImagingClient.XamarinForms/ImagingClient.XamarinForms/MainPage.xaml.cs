19e46aa7ecb757451fd24972b53cefdcc362f859
Josh Weeks <jweeks@idnetworks.com>
2018-04-09 11:40:09 -0400

    add Xamarin.Forms tests

---
+++ ImagingClient.XamarinForms/ImagingClient.XamarinForms/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Imaging;
+using System.IO;
+using Xamarin.Forms;
+
+namespace ImagingClient.XamarinForms
+{
+    public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            var assembly = this.GetType().Assembly;
+            var webpStream = assembly.GetManifestResourceStream("ImagingClient.XamarinForms.fireguy.webp");
+            byte[] webp;
+            using (var ms = new MemoryStream())
+            {
+                webpStream.CopyTo(ms);
+                webp = ms.ToArray();
+            }
+            byte[] png = ImageConvert.WebpToPng(webp);
+            this.Img.Source = ImageSource.FromStream(() => new MemoryStream(png));
+		}
+	}
+}
